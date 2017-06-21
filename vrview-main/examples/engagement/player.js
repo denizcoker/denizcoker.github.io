6494071f6323be188dd7f605eeed3f81557d8596
denizcoker <denizcoker@users.noreply.github.com>
2017-06-21 11:25:24 -0700

    Update player.js

--- vrview-main/examples/engagement/player.js
+++ vrview-main/examples/engagement/player.js
@@ -33,7 +33,7 @@ function onLoad() {
     //is_debug: true,
     //default_heading: 90,
     //is_yaw_only: true,
-    //is_vr_off: true,
+    is_vr_off: true,
   });
 
   playButton = document.querySelector('#toggleplay');
